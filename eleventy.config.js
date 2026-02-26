e622b7a2e7de2b26aeb26237bc57db6ef377eed0
lam <lam@bitcoinvn.io>
2026-02-26 13:14:34 +0700

    First commit

---
+++ eleventy.config.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+  // Pass through static assets
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/assets');
+
+  // Add a "year" shortcode for copyright
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+      includes: '_includes',
+      data: '_data',
+    },
+    templateFormats: ['njk', 'html'],
+    htmlTemplateEngine: 'njk',
+  };
+};
